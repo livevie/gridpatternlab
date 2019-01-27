a043d6b8af971f0d3fcc7a688934029dfc40c937
livevie <ela661991@gmail.com>
2019-01-27 19:42:42 +0100

    new component: multicolumns

--- js/multicolumn.js
+++ js/multicolumn.js
@@ -1,60 +1,43 @@
 const support = [
-  'IE10+, Firefox 2+, Chrome 1+, Edge 12, Safari 3.1',
-  'IE11+, Firefox 22+, Chrome 29+, Edge 12, Safari 9',
-  'Firefox 52+, Chrome 57+, Edge 16+, Safari 10.1'
+    'IE10+, Firefox 2+, Chrome 1+, Edge 12, Safari 3.1',
+    'IE11+, Firefox 22+, Chrome 29+, Edge 12, Safari 9',
+    'Firefox 52+, Chrome 57+, Edge 16+, Safari 10.1'
 ];
 const css = `body {
     background: #fff;
-  }
-  
-  .gallery {
-      /* only2 */
-      display: flex;
-      flex-wrap: wrap;
-      /* endonly2 */
-      
-      /* only3 */
-      display: grid;
-      grid-gap: 20px;
-      grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-      grid-auto-rows: 200px;
-      /* endonly3 */
-  }
-
-  /* only1 */
-  .gallery::after {
-      content: "";
-      display: block;
-      clear: both;
-      visibility: hidden;
-      height: 0;
-  }
-  /* endonly1 */
-  
-  /* only1 */
-  .photo {
-      float: left;
-  }
-  /* endonly1 */
-  
-  /* until2 */
-  .photo {
-      width: 250px;
-      height: 200px;
-      margin: 10px;
-      flex-grow: 1;
-  }
-  /* enduntil2 */
-  
-  /* only2 */
-  @supports (display: grid) {
-      .photo {
-          width: auto;
-          height: auto;
-          margin: 0;
+  } 
+      .multicolum__elements {
+          /* only2 */
+          display: flex;
+          flex-wrap: wrap;
+          /* endonly2 */
+          /* only3 */
+          display: grid;
+          grid-auto-columns: minmax(auto,250px);
+          grid-auto-flow: column;
+          grid-template-rows: repeat(5,auto);
+          /* endonly3 */
       }
-  }
-  /* endonly2 */
+      
+       /* only1 */
+          .multicolum__elements::after {
+              content: "";
+              display: block;
+              clear: both;
+              visibility: hidden;
+              height: 0;
+        }
+        /* endonly1 */
+      
+           /* until2 */
+          .multicolum-element {
+             width: 250px;
+             /* only1 */
+             float: left;
+             /* endonly1 */
+          }
+          /* enduntil2 */
+      
 `;
 
 // Supporttext Ausgabeelement
@@ -65,61 +48,61 @@ const support_slider = document.querySelector('.js-support-selection');
 const support_toggle = document.querySelector('.js-support-toggle');
 
 function trim_comments(string) {
-  // Regex um Kommentare zu entfernen
-  // Kommentare entfernen
-  const trim_comments = new RegExp(
-    `\\/\\*(.\[\\s\\S\]*?)\\*\\/(\r\n|\n|\r)|`,
-    'g'
-  );
-  let css_no_coment = string.replace(trim_comments, '');
-  // CSS formatieren
-  var beautified = cssbeautify(css_no_coment, {
-    indent: '  ',
-    autosemicolon: true
-  });
+    // Regex um Kommentare zu entfernen
+    // Kommentare entfernen
+    const trim_comments = new RegExp(
+        `\\/\\*(.\[\\s\\S\]*?)\\*\\/(\r\n|\n|\r)|`,
+        'g'
+    );
+    let css_no_coment = string.replace(trim_comments, '');
+    // CSS formatieren
+    var beautified = cssbeautify(css_no_coment, {
+        indent: '  ',
+        autosemicolon: true
+    });
 
-  return beautified.trimEnd();
+    return beautified.trimEnd();
 }
 
 /**
- * 
- * @param {string} string 
- * @param {int} support 
- * @param {node} element 
- * 
+ *
+ * @param {string} string
+ * @param {int} support
+ * @param {node} element
+ *
  * CSS passend zur Auswahl anzeigen
  */
 function displayCSS(string, support, element = '.js-css-display') {
-  // CSS ohne Kommentare anzeigen
-  document.querySelector(element).innerHTML = trim_comments(string);
-
-  // Kommentare durchgehen
-  for (var i = 1; i < parseInt(support); i++) {
-    // Alles zwischen /* onlyN */ und /* endonlyN */ lesen und inklusive Kommentar entfernen
-    let removeOnly = new RegExp(
-      `\\/\\* only${i} \\*\\/(\[\\s\\S\]*?)\\/\\* endonly${i} \\*\\/`,
-      'g'
-    );
-    string = string.replace(removeOnly, '');
-    // Alles zwischen /* untilN */ und /* enduntilN */ lesen und inklusive Kommentar entfernen
-    let removeUntil = new RegExp(
-      `\\/\\* until${i} \\*\\/(\[\\s\\S\]*?)\\/\\* enduntil${i} \\*\\/`,
-      'g'
-    );
-    string = string.replace(removeUntil, '');
-
     // CSS ohne Kommentare anzeigen
     document.querySelector(element).innerHTML = trim_comments(string);
-  }
+
+    // Kommentare durchgehen
+    for (var i = 1; i < parseInt(support); i++) {
+        // Alles zwischen /* onlyN */ und /* endonlyN */ lesen und inklusive Kommentar entfernen
+        let removeOnly = new RegExp(
+            `\\/\\* only${i} \\*\\/(\[\\s\\S\]*?)\\/\\* endonly${i} \\*\\/`,
+            'g'
+        );
+        string = string.replace(removeOnly, '');
+        // Alles zwischen /* untilN */ und /* enduntilN */ lesen und inklusive Kommentar entfernen
+        let removeUntil = new RegExp(
+            `\\/\\* until${i} \\*\\/(\[\\s\\S\]*?)\\/\\* enduntil${i} \\*\\/`,
+            'g'
+        );
+        string = string.replace(removeUntil, '');
+
+        // CSS ohne Kommentare anzeigen
+        document.querySelector(element).innerHTML = trim_comments(string);
+    }
 }
 
 /**
- * 
- * @param {int} index 
+ *
+ * @param {int} index
  * Support text anzeigen
  */
 function displaySupport(index) {
-  support_text.textContent = support[index - 1];
+    support_text.textContent = support[index - 1];
 }
 
 // Per default Grid-Support festlegen
@@ -128,10 +111,10 @@ displayCSS(css, support_slider.value);
 
 // Wenn der Regler bewegt wird
 support_slider.addEventListener('change', e => {
-  const support = e.target.value;
+    const support = e.target.value;
 
-  displaySupport(support);
-  displayCSS(css, support);
+    displaySupport(support);
+    displayCSS(css, support);
 });
 
 // Support-Steuerung erstellen
