1139dd163c6faea94f5c98fa93edf57685d60115
Manuel Matuzovic <manuel@matuzo.at>
2019-02-13 09:55:24 +0100

    Add trimend polyfill

--- js/renderComponent.js
+++ js/renderComponent.js
@@ -1,3 +1,14 @@
+String.prototype.trimEnd = String.prototype.trimEnd ? String.prototype.trimEnd : function() {
+	if(String.prototype.trimRight) {
+		return this.trimRight();
+	} else if(String.prototype.trim) {
+		var trimmed = this.trim();
+		var indexOfWord = this.indexOf(trimmed);
+		
+		return this.slice(indexOfWord, this.length);
+	}
+};
+
 // Supporttext Ausgabeelement
 const support_text = document.querySelector('.js-support');
 // Slider
